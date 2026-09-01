62d15a06a55f722773d96c490bd7a21f54fe314d
Pratik09988 <ghumaliyapratik999@gmail.com>
2026-09-01 20:53:15 +0530

    Update Artistitem.jsx

--- Spotify-clone/src/components/Artistitem.jsx
+++ Spotify-clone/src/components/Artistitem.jsx
@@ -5,10 +5,10 @@ const Artistitem = ({ name, image, desc, id }) => {
     const navigate = useNavigate();
 
     return (
-        <div onClick={() => navigate(`/artist/${id}`)} className="cursor-pointer w-[calc(50vw-24px)] min-w-[calc(50vw-24px)] sm:min-w-[180px] sm:w-auto p-2 px-3 rounded hover:bg-[#ffffff26]">
-            <img className="rounded w-full aspect-square object-cover" src={image} alt={name} />
-            <p className="font-bold mt-2 mb-1 truncate">{name}</p>
-            <p className="text-slate-400 text-sm truncate">{desc}</p>
+        <div onClick={() => navigate(`/artist/${id}`)} className="cursor-pointer min-w-[180px] p-2 px-3 rounded hover:bg-[#ffffff26]">
+            <img className="rounded" src={image} alt={name} />
+            <p className="font-bold mt-2 mb-1">{name}</p>
+            <p className="text-slate-400 text-sm">{desc}</p>
         </div>
     );
 };

62d37ffe6958ea47f21961510198aa48366afc28
Pratik09988 <ghumaliyapratik999@gmail.com>
2026-09-01 20:47:34 +0530

    Update Albumitem.jsx

--- Spotify-clone/src/components/Albumitem.jsx
+++ Spotify-clone/src/components/Albumitem.jsx
@@ -5,10 +5,10 @@ const Albumitem = ({ name, image, desc, id }) => {
     const navigate = useNavigate();
 
     return (
-        <div onClick={() => navigate(`/album/${id}`)} className="cursor-pointer w-[180px] min-w-[180px] max-w-[180px] p-2 px-3 rounded hover:bg-[#ffffff26]">
-            <img className="w-full aspect-square rounded object-cover" src={image} alt={name} />
+        <div onClick={() => navigate(`/album/${id}`)} className="cursor-pointer w-[calc(50vw-24px)] min-w-[calc(50vw-24px)] sm:min-w-[180px] sm:w-auto p-2 px-3 rounded hover:bg-[#ffffff26]">
+            <img className="rounded w-full aspect-square object-cover" src={image} alt={name} />
             <p className="font-bold mt-2 mb-1 truncate">{name}</p>
-            <p className="text-slate-400 text-sm line-clamp-2">{desc}</p>
+            <p className="text-slate-400 text-sm truncate">{desc}</p>
         </div>
     );
 };
